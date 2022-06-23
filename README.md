7d327cfa1fcd0056bfe3643b83b304bc237b7cb8
mallocreal <76497029+miloud2000@users.noreply.github.com>
2022-06-23 11:24:15 +0100

    commit

---
+++ README.md
@@ -0,0 +1,2 @@
+projet pfa 
+land registry
---
+++ Server/backend/Config/db_config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  email: "echarkimiloud18@gmail.com",
+  password: "Miloud@1",
+  NEXMO_API_KEY: "Ad07806cf",
+  NEXMO_API_SECRET: "lbK2OrEPthnvzAGj",
+  NEXMO_FROM_NUMBER: "23",
+  MongoURI: `mongodb://akira:qwerty123@ac-7r4wl7z-shard-00-00.0u1q8wb.mongodb.net:27017,ac-7r4wl7z-shard-00-01.0u1q8wb.mongodb.net:27017,ac-7r4wl7z-shard-00-02.0u1q8wb.mongodb.net:27017/test?ssl=true&replicaSet=atlas-nxkog6-shard-0&authSource=admin&retryWrites=true&w=majority`
+}
